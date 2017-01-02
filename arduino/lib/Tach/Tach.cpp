9f0dd89cfc8db30b872c7279d5903763545e031a
Dennis Dunn <ansofive@gmail.com>
2017-01-01 22:17:16 -0500

    What to say.  It's not working.

--- arduino/lib/Tach/Tach.cpp
+++ arduino/lib/Tach/Tach.cpp
@@ -3,10 +3,10 @@
 
 #define IRQ_PER_REVOLUTION 20
 
-Tach::Tach(int pin, void(*dispatcher)(void))
+Tach::Tach(int pin, void (*dispatcher)(void))
 {
     pinMode(pin, INPUT_PULLUP);
-    attachInterrupt(pin - 2, dispatcher, RISING);
+    attachInterrupt(pin - 2, dispatcher, FALLING);
     _reset();
 }
 
@@ -17,11 +17,14 @@ void Tach::handler()
 
 int Tach::get_rpm()
 {
-    long t = millis() - _t;
-    int n = _n;
-    _reset();
+    noInterrupts();
 
-    return n * 3000 / t;
+    int n = _n;
+    int t = (int)(millis() - _t);
+
+    _reset();
+    interrupts();
+    return (n * 3000) / t;
 }
 
 void Tach::_reset()
