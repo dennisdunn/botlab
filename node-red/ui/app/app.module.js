3ffe4a91cff16522a63837da98dc33bf5d6d5dc3
Dennis Dunn <ansofive@gmail.com>
2017-01-04 22:05:14 -0500

    Initial import of hybrid UI.

---
+++ node-red/ui/app/app.module.js
@@ -0,0 +1,5 @@
+
+angular.module('botconApp', [
+    'ngMaterial',
+    'controlConsole'
+]);
---
+++ node-red/ui/app/control-console/control-console.component.js
@@ -0,0 +1,9 @@
+
+angular
+.module("controlConsole")
+.component("controlConsole", {
+    templateUrl:"control-console/control-console.template.html",
+    controller:function ControlConsoleController(){
+        
+    }
+});
