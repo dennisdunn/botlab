62429c142c473038c749bef24158b55b9546fcd5
Dennis Dunn <ansofive@gmail.com>
2017-02-07 14:37:07 -0500

    Moved the template to the header.

--- arduino/tachometer/tachometer.h
+++ arduino/tachometer/tachometer.h
@@ -14,6 +14,21 @@ void timer_isr();
 void tach_0_isr();
 void tach_1_isr();
 void send(int x, int y);
-int cmp(const void *arg1, const void *arg2);
+
+// http://arduino.stackexchange.com/questions/13255/do-i-need-a-bubble-sort-or-something-easier
+template <typename T>
+int cmp(const void *arg1, const void *arg2)
+{
+  T *a = (T *)arg1; // cast to pointers to type T
+  T *b = (T *)arg2;
+  // a less than b?
+  if (*a < *b)
+    return -1;
+  // a greater than b?
+  if (*a > *b)
+    return 1;
+  // must be equal
+  return 0;
+}
 
 #endif
--- arduino/tachometer/tachometer.ino
+++ arduino/tachometer/tachometer.ino
@@ -53,23 +53,6 @@ void send(int x, int y)
   Serial.println();
 }
 
-// Sort
-// http://arduino.stackexchange.com/questions/13255/do-i-need-a-bubble-sort-or-something-easier
-template <typename T>
-int cmp(const void *arg1, const void *arg2)
-{
-  T *a = (T *)arg1; // cast to pointers to type T
-  T *b = (T *)arg2;
-  // a less than b?
-  if (*a < *b)
-    return -1;
-  // a greater than b?
-  if (*a > *b)
-    return 1;
-  // must be equal
-  return 0;
-}
-
 // ISR
 
 void timer_isr()
