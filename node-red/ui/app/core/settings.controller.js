55e237525a04d96f22e08e14544157eebbb4fbcb
Dennis Dunn <ansofive@gmail.com>
2017-02-15 21:06:07 -0500

    Wipe the UI, gonna try something else.

--- node-red/ui/app/core/settings.controller.js
+++
@@ -1,7 +0,0 @@
-angular.module('core')
-    .controller('SettingsController', function ($scope, $mdDialog, settings) {
-        $scope.settings = settings;
-        $scope.close = function () {
-            $mdDialog.hide();
-        };
-    });
--- node-red/ui/app/core/settings.service.js
+++
@@ -1,8 +0,0 @@
-angular.module('core')
-    .factory('settingsService', function () {
-        return {
-            botUri: 'http://roverbot:1880',
-            apiUri: '/api',
-            editUri: '/admin'
-        }
-    });
