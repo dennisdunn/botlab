9d3d64e0ab937a8bf9d9f6fa6ab508b16ac11a60
Dennis Dunn <ansofive@gmail.com>
2017-02-19 08:24:26 -0500

    First stab at tachometer reader.

--- qbot/tach.py
+++ qbot/tach.py
@@ -5,12 +5,24 @@ import sys
 import getopt
 import serial
 import pika
-
+import atexit
 
 def main(argv):
     """ Read tachometer values"""
     options = parseoptions(argv)
-    port = openport(options)
+    options["port"] = openport(options)
+    options["queue"] = openqueue(options)
+
+    atexit.register(close, options)
+
+    while(1):
+        #read serial(
+        data = port.readLine()
+        vals = data.split(",")
+        for in range(0,1):
+            msg = {}
+            msg["tach_{}".format(i))] = int(vals[i])
+            channel.basic_publish(exchange='', routing_key=options["queueName"], body=msg)
 
 
 def parseoptions(argv):
@@ -30,7 +42,7 @@ def parseoptions(argv):
         elif opt in ("-b", "--baud"):
             options["baud"] = arg
         elif opt in ("-q", "--queue"):
-            options["queue"] = arg
+            options["queueName"] = arg
     return options
 
 def openport(options):
@@ -38,14 +50,23 @@ def openport(options):
     ser = serial.Serial(
         port=options["device"],
         baudrate=options["baud"],
-        parity=serial.PARITY_ODD,
-        stopbits=serial.STOPBITS_TWO,
-        bytesize=serial.SEVENBITS
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS
     )
     return ser
 
 def openqueue(options):
     """Connect to the tachometer queue"""
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue=options[queueName])
+    return channel
+
+def close(options):
+    """Cleanup"""
+    options["port"].close()
+    options["queue"].close()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
