e8bdf1ebc29cdc683c3fdf2ed43a1c09249f65d8
Dennis Dunn <ansofive@gmail.com>
2017-02-21 23:41:50 -0500

    Make it a simple script and use the process module to wait for SIGINT.

--- qbot/tach.js
+++ qbot/tach.js
@@ -3,7 +3,10 @@
 'use strict'
 
 let process = require('process');
+let serial = require('serialport');
+let amqp = require('amqplib')
 
+let channel = null;
 
 const serialOptions = {
     baud: 115200,
@@ -11,60 +14,91 @@ const serialOptions = {
 };
 
 const queueOptions = {
-    queue: 'tach_sensors',
-    callback: (data) => { console.log(data) }
+    queue: 'tach_sensors'
 };
 
-class PortMonitor {
-    constructor(options = serialOptions) {
-        let serial = require('serialport');
-        this.settings = options;
-        this.port = new serial(options.device, {
-            baudRate: options.baud,
-            parser: serial.parsers.readline('\n')
-        });
-        this.port.on('error', function (err) {
-            console.log('Error: ', err.message);
-        })
-        this.port.on('data', this.transform);
-    }
 
-    transform(data) {
+amqp.connect('amqp://localhost').then(conn => {
+    return conn.createChannel();
+}).then(ch => {
+    channel = ch;
+}).catch(console.warn);
+
+
+let port = new serial(serialOptions.device, {
+    baudRate: serialOptions.baud,
+    parser: serial.parsers.readline('\n')
+});
+port.on('error', (err) => {
+    console.log(err);
+})
+port.on('data', (data) => {
+    try {
+        data = data.trim();
         let counts = JSON.parse(data);
+
         for (let i = 0; i < counts.length; i++) {
             let msg = { topic: 'tach_reading', id: i, value: counts[i] };
-            if (this.settings.callback) this.settings.callback(msg);
+            let buffer = Buffer.from(JSON.stringify(msg));
+            channel.assertQueue(queueOptions.queue).then(function (ok) {
+                return channel.sendToQueue(queueOptions.queue, buffer);
+            });
         }
     }
-};
-
-class Publisher {
-    constructor(options = queueOptions) {
-        this.settings = options;
-        let self = this;
-        let open = require('amqplib').connect('amqp://localhost');
-
-        open.then(function (conn) {
-            return conn.createChannel();
-        }).then(function (ch) {
-            self.channel = ch;
-        }).catch(console.warn);
+    catch (e) {
+        // do nothing, more data is comming soon
     }
+});
 
-    publish(data) {
-        console.log(data);
-        let buffer = Buffer.from(JSON.stringify(data));
+// class PortMonitor {
+//     constructor(options = serialOptions) {
+//         this.settings = options;
+//         this.port = new serial(options.device, {
+//             baudRate: options.baud,
+//             parser: serial.parsers.readline('\n')
+//         });
+//         this.port.on('error', function (err) {
+//             console.log('Error: ', err.message);
+//         })
+//         this.port.on('data', this.transform);
+//     }
 
-        return this.channel.assertQueue(this.settings.queue).then(function (ok) {
-            return this.channel.sendToQueue(this.settings.queue, buffer);
-        });
-    }
-};
+//     transform(data) {
+//         let counts = JSON.parse(data);
+//         for (let i = 0; i < counts.length; i++) {
+//             let msg = { topic: 'tach_reading', id: i, value: counts[i] };
+//             if (this.settings.callback) this.settings.callback(msg);
+//         }
+//     }
+// };
 
-//let publisher = new Publisher(queueOptions);
-//serialOptions.callback = publisher.publish;
-let mon = new PortMonitor(serialOptions);
+// class Publisher {
+//     constructor(options = queueOptions) {
+//         this.settings = options;
+//         let self = this;
+//         let open = require('amqplib').connect('amqp://localhost');
 
-process.on('SIGINT',()=>{
+//         open.then(function (conn) {
+//             return conn.createChannel();
+//         }).then(function (ch) {
+//             self.channel = ch;
+//         }).catch(console.warn);
+//     }
+
+//     publish(data) {
+//         console.log(data);
+//         let buffer = Buffer.from(JSON.stringify(data));
+
+//         return this.channel.assertQueue(this.settings.queue).then(function (ok) {
+//             return this.channel.sendToQueue(this.settings.queue, buffer);
+//         });
+//     }
+// };
+
+// //let publisher = new Publisher(queueOptions);
+// //serialOptions.callback = publisher.publish;
+// let mon = new PortMonitor(serialOptions);
+
+process.on('SIGINT', () => {
     process.exit();
 });
