0101129d0ef1791109ddfe4a9690dd6db97095c3
Dennis Dunn <ansofive@gmail.com>
2017-02-27 22:41:57 -0500

    Motor handler set/gets motor state.

--- qbot/lib/motorhandler.js
+++ qbot/lib/motorhandler.js
@@ -3,22 +3,27 @@
 module.exports = (gpioService => {
 
     const _service = gpioService;
+    const _motors = [{ 'power': 0 }, { 'power': 0 }];
 
     const self = {
         list: (cmd) => {
-            return {};
+            return _motors;
         },
 
         get: (cmd) => {
-            return {};
+            return _motors[mKey];
         },
 
         set: (cmd) => {
-            return {};
+            _motors[cmd.key].power = cmd.value;
+            return self.get(cmd);
         },
 
         setall: (cmd) => {
-            return {};
+            for (var i = 0; i < _motors.length; i++) {
+                self.set({ 'key': i, 'value': cmd.value });
+            }
+            return self.list();
         }
     }
     return self;
