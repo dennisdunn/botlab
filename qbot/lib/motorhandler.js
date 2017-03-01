812d171dfccf2fee8343a086ec045ebef470e06c
Dennis Dunn <ansofive@gmail.com>
2017-02-28 21:26:34 -0500

    Motor handler.

--- qbot/lib/motorhandler.js
+++ qbot/lib/motorhandler.js
@@ -2,8 +2,40 @@
 
 module.exports = (gpioService => {
 
+    const MOTOR_A_KEY = 'A';
+    const MOTOR_B_KEY = 'B';
+
+    const MOTOR_A_FORWARD = 19;
+    const MOTOR_A_BACKWARD = 20;
+    const MOTOR_B_FORWARD = 21;
+    const MOTOR_B_BACKWARD = 26;
+
+    const DIRECTION_FORWARD = 'forward';
+    const DIRECTION_BACKWARD = 'backward';
+
+    const MOTOR_GPIO = {
+        MOTOR_A_KEY: {
+            DIRECTION_FORWARD: MOTOR_A_FORWARD,
+            DIRECTION_BACKWARD: MOTOR_A_BACKWARD
+        },
+        MOTOR_B_KEY: {
+            DIRECTION_FORWARD: MOTOR_B_FORWARD,
+            DIRECTION_BACKWARD: MOTOR_B_BACKWARD
+        }
+    }
+
     const _service = gpioService;
-    const _motors = [{ 'power': 0 }, { 'power': 0 }];
+
+    const _motors = {
+        MOTOR_A_KEY: {
+            'power': 0,
+            'direction': DIRECTION_FORWARD
+        },
+        MOTOR_B_KEY: {
+            'power': 0,
+            'direction': DIRECTION_FORWARD
+        }
+    }
 
     const self = {
         list: (cmd) => {
@@ -11,17 +43,24 @@ module.exports = (gpioService => {
         },
 
         get: (cmd) => {
-            return _motors[cmd.key];
+            return _motors[cmd.key.toUpperCase()];
         },
 
         set: (cmd) => {
-            _motors[cmd.key].power =parseInt( cmd.value);
+            let level = parseInt(cmd.value);
+            level = level > 255 ? 255 : level < 0 ? 0 : level;
+            _motors[cmd.key.toUpperCase()].power = level;
+            let direction = _motors[key]['direction'];
+            let signal = MOTOR_GPIO[key][direction];
+            let gpio = new gpio(signal, , { mode: Gpio.OUTPUT });
+            gpio.pwmWrite(level);
             return self.get(cmd);
         },
 
         setall: (cmd) => {
-            for (var i = 0; i < _motors.length; i++) {
-                self.set({ 'key': i, 'value': cmd.value });
+            for (var key of Object.keys(_motors)) {
+                let cmd = { 'key': key, 'value': cmd.value };
+                self.set(cmd);
             }
             return self.list();
         }
