99d0ba4c4e1a45fbf41766dea603e3acbddd5206
Dennis Dunn <ansofive@gmail.com>
2017-03-24 23:14:01 -0400

    Fixing a merge thingy.

--- qbot/ui/app.js
+++ qbot/ui/app.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import AppContainer from './appContainer';
-import reducer from './lib/reducers';
+import reducers from './lib/reducers';
 import { Provider } from 'react-redux';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
-const store = createStore(reducer);
+const store = createStore(reducers);
 
 injectTapEventPlugin();
 
--- qbot/ui/appContainer.js
+++ qbot/ui/appContainer.js
@@ -2,24 +2,32 @@ import React from 'react'
 import { connect } from 'react-redux'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import ControlGrid from './components/controlGrid'
+import ActionFactory from './lib/actionCreators'
+import * as Actions from './lib/actions'
 
 const url = 'http://'
 
 class AppContainer extends React.Component {
 
     constructor(props) {
-        super(props);
+        super(props)
+        this.clickHandler = this.clickHandler.bind(this)
+    }
+
+    clickHandler(eventArgs) {
+        let { dispatch } = this.props
+        let action = ActionFactory[eventArgs.action](eventArgs)
+        dispatch(action)
     }
 
     render() {
         return (
             <MuiThemeProvider>
                 <div style={{ position: 'relative' }}>
-                    <ControlGrid id='controlGrid' size='310' onClick={console.log}></ControlGrid>
+                    <ControlGrid id='controlGrid' size='310' onClick={this.clickHandler}></ControlGrid>
                 </div>
             </MuiThemeProvider>
         );
     }
 }
-
 export default connect(state => state)(AppContainer)
