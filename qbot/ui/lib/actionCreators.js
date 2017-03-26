2b278231013bf51d5124c0fae04447e25da729de
Dennis Dunn <ansofive@gmail.com>
2017-03-25 22:04:10 -0400

    Using fetch to post to the bot.

--- qbot/ui/lib/actionCreators.js
+++ qbot/ui/lib/actionCreators.js
@@ -1,10 +1,12 @@
 import * as Actions from './actions'
-
+require('es6-promise').polyfill()
+require('isomorphic-fetch')
 /**
  * Actions need a target (led, motor), key (red, blue, A, etc),
  * and value (on, off, 150, etc)
  */
 let factory = {}
+const urlBase = 'http://192.168.0.13:8080/api/v1/'
 
 factory[Actions.SWITCH_ON] = (eventArgs) => {
     return {
@@ -25,10 +27,23 @@ factory[Actions.SWITCH_OFF] = (eventArgs) => {
 factory[Actions.TOGGLE_SWITCH] = (eventArgs) => {
     return (dispatch, getstate) => {
         let current = getstate()['switchReducer'][eventArgs.payload]
-        dispatch({
-            type: current ? Actions.SWITCH_OFF : Actions.SWITCH_ON,
-            target: 'led',
-            key: eventArgs.payload
+        fetch(urlBase + 'led/' + eventArgs.payload, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                value: !current
+            })
+        })
+        .then(checkStatus)
+        .then(parseJSON)
+        .then(data => {
+            dispatch({
+                type: current ? Actions.SWITCH_OFF : Actions.SWITCH_ON,
+                target: 'led',
+                key: eventArgs.payload
+            })
         })
     }
 }
@@ -67,6 +82,20 @@ factory[Actions.SET_TURN_OFF] = (eventArgs) => {
         type: Actions.SET_TURN_OFF,
         payload: null
     }
+}
+
+function checkStatus(response)  {
+    if (response.status >= 200 && response.status < 300) {
+        return response
+    } else {
+        var error = new Error(response.statusText)
+        error.response = response
+        throw error
+    }
+}
+
+function parseJSON(response) {
+  return response.json()
 }
 
 export default factory
