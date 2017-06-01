f9457935276a8cc736a1ec90164b30aa05bfc9e2
Dennis Dunn <ansofive@gmail.com>
2017-05-31 21:53:18 -0400

    Added some files that didn't get added by the merge.

---
+++ pybot/dist/rest.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+from flask import Flask, request, redirect, json, jsonify
+
+from flask_cors import CORS
+from motor import Motor
+
+app = Flask(__name__)
+CORS(app)
+
+motorCtl = Motor()
+
+@app.route("/", methods=['GET'])
+def index():
+    return redirect("/static/index.html", code=302)
+
+
+@app.route("/api/v1/steering/<cmd>", methods=['POST'])
+def steering(cmd):
+    if cmd == "left":
+        motorCtl.left()
+    elif cmd == "right":
+        motorCtl.right()
+    elif cmd == "straight":
+        motorCtl.straight()
+    return jsonify(cmd)
+
+
+@app.route("/api/v1/motor/<cmd>", methods=['POST'])
+def motor(cmd):
+    if cmd == "forward":
+        motorCtl.forward()
+    elif cmd == "reverse":
+        motorCtl.reverse()
+    return jsonify(cmd)
+
+@app.route("/api/v1/throttle", methods=['POST'])
+def throttle():
+    data = json.loads(request.data)
+    motorCtl.throttle(data)
+    return jsonify(data)
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
---
+++ pybot/dist/sonar.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Constantly ping and when collision is immenent,
+# take control to avoid the obstacle.
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
