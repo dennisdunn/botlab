709855bd8682106deaae4754f6dc11b6c378c222
Dennis Dunn <ansofive@gmail.com>
2017-07-25 22:31:27 -0400

    Added an Elixir robot project.

--- scooter/mix.exs
+++ scooter/mix.exs
@@ -1,10 +1,10 @@
-defmodule Bot.Mixfile do
+defmodule Scooter.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :bot,
+    [app: :scooter,
      version: "0.1.0",
-     elixir: "~> 1.3",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
@@ -14,16 +14,18 @@ defmodule Bot.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    # Specify extra applications you'll use from Erlang/Elixir
+    [extra_applications: [:logger],
+     mod: {Scooter.Application, []}]
   end
 
   # Dependencies can be Hex packages:
   #
-  #   {:mydep, "~> 0.3.0"}
+  #   {:my_dep, "~> 0.3.0"}
   #
   # Or git/path repositories:
   #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
   #
   # Type "mix help deps" for more examples and options
   defp deps do
