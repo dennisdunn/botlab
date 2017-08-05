1259ce11640ab78619ecb5bde398fde8721c07b0
Dennis Dunn <ansofive@gmail.com>
2017-08-05 00:11:51 -0400

    Trying to use agents but there is an underlying genserver that is barfing on handle info calls.

--- scooter/apps/pigpio/lib/pigpio.ex
+++ scooter/apps/pigpio/lib/pigpio.ex
@@ -1,18 +1,82 @@
 defmodule Pigpio do
+  use Agent
+
   @moduledoc """
-  Documentation for Pigpio.
+  The module uses the pipe interface of the
+  pigpiod daemon to control the GPIO pins on a 
+  Raspberry Pi.
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Pigpio.hello
-      :world
-
+  Start the link with the pigpio pipes.
   """
-  def hello do
-    :world
+  def start_link() do
+    fn -> %{
+      io: Port.open('/dev/pigpio', [:eof]),
+      out: Port.open('/dev/pigout', [:eof]),
+      err: Port.open('/dev/pigerr', [:eof])
+    } end
+    |> Agent.start_link(name: __MODULE__) 
+  end
+
+  @doc """
+  Read the value of a GPIO pin.
+  """
+  def read(pin) do
+    command "r #{pin}\n"
+  end
+
+  @doc """
+  Set the value of the GPIO pin to the given value.
+  """
+def write(pin, level) do
+  command "w #{pin} #{level}\n"
+end
+
+@doc """
+Execute a pigpiod command by sending the command
+the daemon via the input pipe and reading the output pipe.
+If an error occurred, also read the error pipe.
+"""
+defp command(cmd) do
+  get_port(:io)
+  |> Port.command(cmd)
+  out = get_port :out
+  receive do
+    {^out, {:data, data}} ->
+      errcode = reply_to_integer data
+      if errcode < 0 do
+        err = get_port :err
+        receive do
+          {^err, {:data, msg}} ->
+            {:error, errcode, msg}
+        after 1_000 ->
+          :timeout
+        end
+      else
+        {:ok, errcode}
+      end        
+  after 1_000 ->
+    :timeout
   end
 end
+
+@doc """
+Get the indicated port from the agent state.
+"""
+defp get_port(key) do
+  Agent.get __MODULE__, fn ports -> ports[key] end
+end
+
+@doc """
+Parse the string to an int.
+"""
+defp reply_to_integer(chars) do
+  chars
+  |> to_string
+  |> String.trim
+  |> String.to_integer
+end  
+end
+
+
--- scooter/apps/pigpio/test/pigpio_test.exs
+++ scooter/apps/pigpio/test/pigpio_test.exs
@@ -3,22 +3,27 @@ defmodule PigpioTest do
   doctest Pigpio
 
   test "reads a GPIO pin" do
+    Pigpio.start_link()
     assert Pigpio.read(4) == 0
   end
 
   test "writes to a GPIO pin" do
+    Pigpio.start_link()
     assert Pigpio.write(4, 0) == 0
   end
 
   test "starts PWM on a GPIO pin" do
+    Pigpio.start_link()
     assert Pigpio.pwm(4, 255) == 0
   end
 
   test "positions a servo on a GPIO pin" do
+    Pigpio.start_link()
     assert Pigpio.servo(4, 1000) == 0
   end
 
   test "monitors the state of a pin" do
+    Pigpio.start_link()
     assert Pigpio.monitor(4) == 0
   end
 end
